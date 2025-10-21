ba46aa836fd453d76a35bc0af3f735cfd52ae4aa
aimee <champ4gnec0ast@gmail.com>
2025-10-21 09:42:54 +0100

    adding silly input mechanism yayayyayaay

--- caesar.c
+++ caesar.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    
+    char letters[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    char message[101]; 
+    printf("message yayay: ");
+    fgets(message, sizeof(message), stdin);
+    message[strcspn(message, "\n")] = '\0';
+
+    printf("You entered: %s\n", message);
+    return 0;
+
 }
