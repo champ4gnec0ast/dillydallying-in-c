73771dce0ccd492c611f60e83fa6e2dbc7e7838a
aimee <champ4gnec0ast@gmail.com>
2025-10-21 10:47:25 +0100

    yayyyyy working with input validation & both positive & negative shifts

--- caesar.c
+++ caesar.c
@@ -3,24 +3,35 @@
 
 int main() {
     //declaring relevant variables for program
-    char letters[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    char message[4] = {'A', 'O', 'H', 'N'};
-    int shift = 3;
-    char dir = '-';
+    char letters[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}; 
+    char message[101]; 
+    char temp[2];
+    int shift;
+    char dir;
+
+    //taking message
+    printf("message yayay: ");
+    fgets(message, sizeof(message), stdin);
+    message[strcspn(message, "\n")] = '\0';
+    printf("You entered: %s\n", message);
+
+    //declaring output size
+    char output[sizeof(message) + 1];
+
+    //taking shift value & direction
+    printf("shift & dir yayayay: ");
+    scanf("%d %c", &shift, &dir);
 
     for (int y = 0; y < strlen(message); y++) {
         for (int x = 0; x < 26; x++) {
             if (letters[x] == message[y]) {
-                x - shift < 0 && dir == '-' ? 
-                printf("%c\n", letters[(x + 26) - shift]) : 
-                x + shift > 26 && dir == '+' ?
-                printf("%c\n", letters[(x + shift) % 26]) :
-                dir == '-' ?
-                printf("%c\n", letters[x-shift]) :
-                printf("%c\n", letters[x+shift]);
+                temp[0] = (dir == '-') ? letters[((x - shift) + 26) % 26 ] : letters[(x + shift) % 26];
+                temp[1] = '\0';
+                strncat(output, temp, 1);
             }
         }
     }
+    printf("%s\n", output);
 
     return 0;
 
